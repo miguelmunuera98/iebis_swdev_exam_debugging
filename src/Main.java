3b208afd65d2d3444059a3b33cfc590cab911bdf
Miguel <mmunueraest.mis2016@student.ie.edu>
2019-04-11 13:13:51 +0200

    This is the first and only commit, with all the bugs fixed.

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,28 @@ public class Main {
 
     public static void main(String[] args) {
         String emailAddress = "john.doe.mis2016@ie.edu";
-        String formattedEmailAddress = emailAddress.replaceAll(".", "/");
+        emailAddress = emailAddress.replaceAll("\\.", "/");
+
 
         Random random = new Random();
         StringBuffer word = null;
 
-        switch (random.nextInt(2)) {
+        switch (random.nextInt(3)) {
             case 0:
-                word = new StringBuffer('Y');
+                word = new StringBuffer("Y");
+                break;
             case 1:
-                word = new StringBuffer('F');
+                word = new StringBuffer("F");
+                break;
             case 2:
-                word = new StringBuffer('T');
+                word = new StringBuffer("T");
+                break;
         }
 
         word.append('o');
         word.append('u');
         word.append('r');
 
-        System.out.println(word + " " + formattedEmailAddress);
+        System.out.println(word + " " + emailAddress);
     }
 }
